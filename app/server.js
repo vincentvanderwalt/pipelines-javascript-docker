343508cc506f05b7ff4c8852baa9085385194f3e
pcarreiras <pcarreiras@users.noreply.github.com>
2021-04-06 10:46:33 +0100

    testing ci/cd

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world again!\n');
+  res.send('Hello world again!\n And again!\n');
 });
 
 app.listen(PORT, HOST);
